43b1b152baa4c4891a7911984dd851d7fb69bb6b
winepkd <45026388+winepkd@users.noreply.github.com>
2019-03-05 14:05:46 +0900

    Create 486A.cpp

---
+++ 486A.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+long long f_n(long long  n)
+{
+    return ( ( 0 == n % 2 ) ? n / 2 : -( 1 + ( n - 1) / 2 ) );
+}
+
+int main()
+{
+    long long n;
+    long long temp;
+
+    std::cin >> n;
+
+    temp = f_n(n);
+    
+    std::cout << temp << std::endl;
+
+    return 0;
+}
